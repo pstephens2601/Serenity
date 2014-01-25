39bc84eefcfd21c85d1de15bbb25c7b1720a0c0c
Patrick Stephens <pstephens@cnoa.org>
2014-01-24 17:07:49 -0800

    Update curl class and add basic css styles.

--- lib/classes/curl.class.php
+++ lib/classes/curl.class.php
@@ -1,6 +1,7 @@
 <?php
 	class curl {
 
+		public $error;
 		private $ch;
 
 		function __construct($url)
@@ -9,9 +10,21 @@
 			curl_setopt($this->ch, CURLOPT_URL, $url);
 		}
 
+		function set_option($option, $value)
+		{
+			curl_setopt($this->ch, $option, $value);
+		}
+
 		public function execute()
 		{
-			curl_exec($this->ch);
+			if ($result = curl_exec($this->ch))
+			{
+				return $result;
+			}
+			else
+			{
+				$this->error = curl_error($this->ch);
+			}
 		}
 
 		function __destruct()
